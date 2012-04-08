3fda5661394ebb6a16359cba596778e765248dad
pussbb <pussbb@gmail.com>
2012-04-08 17:49:12 +0300

    clean up scripts added

--- plugins/projectmanager/obj/ui_projectexplorer.h
+++ plugins/projectmanager/obj/ui_projectexplorer.h
@@ -1,8 +1,8 @@
 /********************************************************************************
 ** Form generated from reading UI file 'projectexplorer.ui'
 **
-** Created: Sun Mar 11 13:49:44 2012
-**      by: Qt User Interface Compiler version 4.8.0
+** Created: Sun Apr 8 17:43:14 2012
+**      by: Qt User Interface Compiler version 4.8.1
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
 ********************************************************************************/
